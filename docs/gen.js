3a9efad6a9e49f080376fb9d1825aa8e683725a3
HenriMalahieude <henrimalahieude@gmail.com>
2022-08-23 12:26:34 -0700

    Can now choose if the view is smooth or not

--- docs/gen.js
+++ docs/gen.js
@@ -1,5 +1,6 @@
 document.getElementById("grid").value = 10
-document.getElementById("resolution").value = 15
+document.getElementById("resolution").value = 30
+document.getElementById("smooth").value = 'off'
 
 let output = document.getElementById("p1");
 let thread_out = document.getElementById("p2")
@@ -8,16 +9,17 @@ let but = document.getElementById("Refresh");
 but.onclick = function(){
     let grid = document.getElementById("grid").value
     let res = document.getElementById("resolution").value
-    
+    let smooth = document.querySelector(".smooth:checked") != null
+
     output.innerHTML = "Grid of " + grid + " x " + grid + " squares @ a resolution of " + res + " x " + res + " per square";
     thread_out.innerHTML = "Gradient Threads: " + (grid) + ", Generation Threads: " + (grid * res);
-    generate(grid, res);
+    generate(grid, res, smooth);
 }
 
-function generate(g, r){
+function generate(g, r, s){
     let canv = document.getElementById("window")
     let ctx = canv.getContext("2d");
     ctx.clearRect(0, 0, canv.clientWidth, canv.clientHeight);
 
-    but.innerHTML = genPerlin(g, r)
+    but.innerHTML = genPerlin(g, r, s)
 }
