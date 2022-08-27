f53aaf20ce36fd011d726d99175968146724e756
HenriMalahieude <henrimalahieude@gmail.com>
2022-08-26 22:52:31 -0700

    Website time

--- docs/gen.js
+++ docs/gen.js
@@ -10,16 +10,18 @@ but.onclick = function(){
     let grid = document.getElementById("grid").value
     let res = document.getElementById("resolution").value
     let smooth = document.querySelector(".smooth:checked") != null
+    let extreme = document.querySelector(".extreme:checked") == null
+    let terrain = document.querySelector(".terrain:checked") != null
 
     output.innerHTML = "Grid of " + grid + " x " + grid + " squares @ a resolution of " + res + " x " + res + " per square";
     thread_out.innerHTML = "Gradient Threads: " + (grid) + ", Generation Threads: " + (grid * res);
-    generate(grid, res, smooth);
+    generate(grid, res, smooth, extreme, terrain);
 }
 
-function generate(g, r, s){
+function generate(g, r, s, e, t){
     let canv = document.getElementById("window")
     let ctx = canv.getContext("2d");
     ctx.clearRect(0, 0, canv.clientWidth, canv.clientHeight);
 
-    but.innerHTML = genPerlin(g, r, s)
+    but.innerHTML = genPerlin(g, r, s, e, t)
 }
